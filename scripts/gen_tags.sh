def33cd4742eba432a6124b5679e118930131b91
Pg <pg.developper.fr@gmail.com>
2025-03-03 13:00:11 +0100

    relan

---
+++ scripts/gen_tags.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Change commit id to tag
+# get all commit and versionName
+python3 -c 'import yaml, re, sys; data = yaml.safe_load(open("./-fdroiddata/metadata/com.deeperwire.ermine.yml")); print("\n".join({f"{i["commit"]} {i["versionName"]}" for i in data["Builds"] if re.fullmatch(r"[0-9a-f]{40}", i["commit"])}))' > tmp.txt
+while read -r SHA1 VERSION; do
+    # Check if a tag exists for this commit
+    TAG_EXISTS=$(git tag --points-at "$SHA1")
+    NEW_TAG="v$VERSION"
+
+    # If no tag exists, create one
+    if [ -z "$TAG_EXISTS" ]; then
+        echo "Creating tag $NEW_TAG for commit $SHA1"
+        git tag "$NEW_TAG" "$SHA1"
+    fi
+    # Replace the SHA-1 commit with the new tag in the YAML file
+    sed -i "s/$SHA1/$NEW_TAG/g" $METADATA
+done < tmp.txt
+rm tmp.txt
