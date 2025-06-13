f0cb157efdaad719400ee021e57dbae1b2669886
relan <email@hidden>
2025-06-13 09:38:42 +0300

    Fennec 139.0.4

--- prebuild.sh
+++ prebuild.sh
@@ -186,7 +186,7 @@ pushd "$application_services"
 # Remove Mozilla repositories substitution and explicitly add the required ones
 patch -p1 --no-backup-if-mismatch --quiet < "$patches/a-c-localize_maven.patch"
 # Break the dependency on older A-C
-sed -i -e '/android-components = /s/"139\.0\.20250417022706"/"139.0"/' gradle/libs.versions.toml
+sed -i -e '/android-components = /s/"139\.0\.20250417022706"/"139.0.4"/' gradle/libs.versions.toml
 echo "rust.targets=linux-x86-64,$rusttarget" >> local.properties
 sed -i -e '/NDK ez-install/,/^$/d' libs/verify-android-ci-environment.sh
 sed -i -e '/content {/,/}/d' build.gradle
