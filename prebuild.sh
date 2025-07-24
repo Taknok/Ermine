52d083d4913de524856d4958a2dd0bdd3287c810
relan <email@hidden>
2025-07-24 11:56:19 +0300

    Bump LLVM to r530567d

--- prebuild.sh
+++ prebuild.sh
@@ -253,8 +253,8 @@ sed -i \
     mobile/android/gradle.py
 
 # Patch the LLVM source code
-# Search clang- in https://android.googlesource.com/platform/ndk/+/refs/tags/ndk-r27/ndk/toolchains.py
-LLVM_SVN='522817'
+# Search clang- in https://android.googlesource.com/platform/ndk/+/refs/tags/ndk-r28b/ndk/toolchains.py
+LLVM_SVN='530567'
 python3 "$toolchain_utils/llvm_tools/patch_manager.py" \
     --svn_version "$LLVM_SVN" \
     --patch_metadata_file "$llvm_android/patches/PATCHES.json" \
