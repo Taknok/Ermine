b92a444a918294959bede4e3962f13c781dd2018
relan <email@hidden>
2026-01-18 10:15:15 +0300

    Fennec 147.0.2

--- prebuild.sh
+++ prebuild.sh
@@ -190,7 +190,7 @@ apply_patch "$patches/a-s-localize_maven.patch"
 # Configure default search engines
 apply_patch "$patches/a-s-configure-default-search-engines.patch"
 # Break the dependency on older A-C
-sed -i -e '/android-components = /s/"145\.0\.2"/"147.0"/' gradle/libs.versions.toml
+sed -i -e '/android-components = /s/"145\.0\.2"/"147.0.2"/' gradle/libs.versions.toml
 echo "rust.targets=linux-x86-64,$rusttarget" >> local.properties
 sed -i -e '/NDK ez-install/,/^$/d' libs/verify-android-ci-environment.sh
 sed -i -e '/content {/,/}/d' build.gradle
